d9efc6f4597ae23f57ed4e9121b12be42438b247
Elissa Sorojsrisom <ess2239@columbia.edu>
2021-02-10 13:34:25 -0500

    updated setup to automatically install dependencies

--- setup.py
+++ setup.py
@@ -15,11 +15,12 @@ from setuptools import setup
 # build command
 setup(
     name="sketchprompt",
-    version="0.0.1",
+    version="0.0.2",
     author="Elissa Sorojsrisom",
     author_email="ess2239@columbia.edu",
     license="GPLv3",
     description="A package to help you fill your sketchbook",
+    install_requires = ["PyObjC", "playsound", "csv", "random", "time"],
     classifiers=["Programming Language :: Python :: 3"],
     entry_points={
         "console_scripts": ["sketchprompt = sketchprompt.__main__:main"]
